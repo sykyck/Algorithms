0d4ee554d4f32d68b76c5f42648d89fe09358ba4
Vaibhav <hihello44694@gmail.com>
2025-03-21 19:31:20 +0530

    Added interview project

---
+++ Interview/MethodHidingAndOverloading.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview
+{
+    public class BaseClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("BaseClass-Method");
+        }
+    }
+
+    public class DerivedClass1 : BaseClass
+    {
+        public new virtual void Method()
+        {
+            Console.WriteLine("Derived1-Method");
+        }
+    }
+
+    public class DerivedClass2 : DerivedClass1
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Derived2-Method");
+        }
+    }
+
+    public class MethodHidingAndOverloading
+    {
+        public static void Main(string[] args)
+        {
+            BaseClass bc = new DerivedClass1();
+            bc.Method();
+            DerivedClass1 dc = new DerivedClass2();
+            dc.Method();
+        }
+    }
+}
